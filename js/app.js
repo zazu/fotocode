dfdbc88041c733b142fc4fca70879cff84ab1bc2
zazu <ralf@goldenzazu.de>
2015-11-15 16:48:26 +0100

    Löschen und Anzeige von Fotos; localstorage

--- js/app.js
+++ js/app.js
@@ -19,6 +19,8 @@ myApp.onPageInit('quickscan', function (page) {
 
 var vm = new Vue({
   el: '#app',
+  myPhotoBrowser: null,
+  selectedSet: null,
   data: {
     sets:[],
     set: {
@@ -29,12 +31,29 @@ var vm = new Vue({
     }
   },
   computed: {
-    numsets: function () {
+    numsets: function() {
         return this.sets.length;
+    },
+    numfotos: function() {
+        var num = 0;
+        for (var i=0; i<this.sets.length; i++) {
+            num = num + this.sets[i].fotos.length;
+        }
+        return num;
+    },
+    numcodes: function() {
+        var num = 0;
+        for (var i=0; i<this.sets.length; i++) {
+            num = num + (this.sets[i].code.length?1:0);
+        }
+        return num;
     },
     emptycode: function () {
         return this.sets.code.length==0;
     }
+  },
+  created: function () {
+    this.sets = Lockr.get('sets',[]);
   },
   methods: {
       cleanset: function() {
@@ -86,8 +105,64 @@ var vm = new Vue({
         }
         me.cleanset();
         mainView.router.load({pageName: 'index'});
+      },
+      removeset: function(idx) {
+          var me = this;
+          myApp.confirm(
+              "Bitte bestätigen Sie das endgültige Löschen des Vorgangs.",
+              "Löschen?",
+              function(){
+                //var i = me.sets.indexOf( me.sets[idx] );
+                me.sets.splice(idx, 1);
+          }, function(){}
+          );
+      },
+      removeFoto: function() {
+          var me = this;
+          myApp.confirm(
+              "Bitte bestätigen Sie das endgültige Löschen des Fotos.",
+              "Löschen?",
+              function(){
+                var idx = me.myPhotoBrowser.activeSlideIndex;
+                me.sets[me.selectedSet].fotos.splice(idx, 1);
+                me.myPhotoBrowser.close();
+                //this.showFotos(this.selectedSet);
+                Lockr.set('sets',me.sets);
+          }, function(){}
+          );
+      },
+      showFotos: function(idx) {
+          var me = this;
+          var photos = [];
+          this.selectedSet = idx;
+          photos = me.sets[idx].fotos.map(function(f){ return f.uri;});
+          this.myPhotoBrowser = myApp.photoBrowser({
+                photos : photos,
+                toolbarTemplate: '\
+                <div class="toolbar tabbar"> \
+                    <div class="toolbar-inner">\
+                        <a href="#" class="link photo-browser-prev">\
+                            <i class="icon icon-prev {{iconsColorClass}}"></i>\
+                        </a>\
+                        <a href="#" onClick="vm.removeFoto();return false;" class="link">\
+                            <i class="icon">Löschen</i>\
+                        </a>\
+                        <a href="#" class="link photo-browser-next">\
+                            <i class="icon icon-next {{iconsColorClass}}"></i>\
+                        </a>\
+                    </div>\
+                </div>'
+          });
+          this.myPhotoBrowser.open(0);
+      },
+      notyet: function() {
+          myApp.alert("Diese Funktion ist noch nicht implementiert.",'appgeordnet');
       }
   }
-})
+});
+
+vm.$watch('sets', function (newVal, oldVal) {
+    Lockr.set('sets',newVal);
+});
 
 
