1558bfc0544f38a451deb48610dec5a51f9d639e
zazu <ralf@goldenzazu.de>
2016-02-04 00:03:06 +0100

    another try for keyboard issue

--- js/app.js
+++ js/app.js
@@ -39,17 +39,12 @@ function onDeviceReady() {
             // device is running Android
             // attach showkeyboard event listener
             // which is triggered when the native keyboard is opened
-            window.addEventListener('native.showkeyboard', keyboardShowHandler);
-
-            // native.showkeyboard callback
-            // e contains keyboard height
-            function keyboardShowHandler(e) {
+            window.addEventListener('native.showkeyboard', function(e) {
                 StatusBar.hide();
                 // get viewport height
                 var viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
                 // get the maximum allowed height without the need to scroll the page up/down
                 var scrollLimit = viewportHeight - (document.activeElement.offsetHeight + document.activeElement.offsetTop);
-
                 // if the keyboard height is bigger than the maximum allowed height
                 if (e.keyboardHeight > scrollLimit) {
                     // calculate the Y distance
@@ -58,23 +53,19 @@ function onDeviceReady() {
                     //move(document.body).to(0, -scrollYDistance).duration('.2s').ease('in-out').end();
                     $$(document.body).scrollTo(0,-scrollYDistance,200);
                 }
-            }
+            });
 
-            window.addEventListener('native.hidekeyboard', keyboardHideHandler);
-
-            // native.hidekeyboard callback
-            function keyboardHideHandler() {
+            window.addEventListener('native.hidekeyboard', function() {
                 StatusBar.hide();
                 // remove focus from activeElement
                 // which is naturally an input since the nativekeyboard is hiding
                 document.activeElement.blur();
                 // animate using move.min.js (CSS3 animations)
                 //move(document.body).to(0, 0).duration('.2s').ease('in-out').end();
-                $$(document.body).scrollTo(0,0,200);
-            }
+                //$$(document.body).scrollTo(0,0,200);
+                $$('.view-main div.page-content').scrollTo(0,scrollYDistance,200);
+            });
         }
-
-
 /*
         window.cordova.plugins.FileOpener.openFile("http://www.website.com/file.pdf",
             function(){
@@ -86,6 +77,23 @@ function onDeviceReady() {
         );
 */
     }
+/*
+    else {
+        window.keyboardShowHandler = function () {
+            keyboardHeight = 500;
+            // get viewport height
+            var viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+            // get the maximum allowed height without the need to scroll the page up/down
+            var scrollLimit = viewportHeight - (document.activeElement.offsetHeight + document.activeElement.offsetTop);
+            // if the keyboard height is bigger than the maximum allowed height
+            if (keyboardHeight > scrollLimit) {
+                // calculate the Y distance
+                var scrollYDistance = document.activeElement.offsetHeight + (keyboardHeight - scrollLimit);
+                $$('.view-main div.page-content').scrollTo(0, scrollYDistance, 200);
+            }
+        }
+    }
+*/
 
     // Initialize the app
     window.myApp = new Framework7({
