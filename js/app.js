306bc66f85bb9e8fb3e659f718f519580a19c363
zazu <ralf@goldenzazu.de>
2016-02-03 23:31:37 +0100

    another try for keyboard issue

--- js/app.js
+++ js/app.js
@@ -26,8 +26,55 @@ function onDeviceReady() {
     if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) {
         window.cfg.device = device;
         window.cfg.version = AppVersion.version;
-        window.addEventListener('native.keyboardshow', function(e){StatusBar.hide();});
-        window.addEventListener('native.keyboardhide', function(e){StatusBar.hide();});
+
+//        window.addEventListener('native.keyboardshow', function(e){StatusBar.hide();});
+//        window.addEventListener('native.keyboardhide', function(e){StatusBar.hide();});
+
+        // fix keyboard hiding focused input texts
+        // using native keyboard plugin and move.min.js
+        // https://github.com/vitohe/ionic-plugins-keyboard/tree/f94842fec1bacf72107083d2e44735e417e8439d
+        // http://visionmedia.github.io/move.js/
+        // not tested on iOS so implementation is for Android only
+        if (device.platform === "Android") {
+            // device is running Android
+            // attach showkeyboard event listener
+            // which is triggered when the native keyboard is opened
+            window.addEventListener('native.showkeyboard', keyboardShowHandler);
+
+            // native.showkeyboard callback
+            // e contains keyboard height
+            function keyboardShowHandler(e) {
+                StatusBar.hide();
+                // get viewport height
+                var viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+                // get the maximum allowed height without the need to scroll the page up/down
+                var scrollLimit = viewportHeight - (document.activeElement.offsetHeight + document.activeElement.offsetTop);
+
+                // if the keyboard height is bigger than the maximum allowed height
+                if (e.keyboardHeight > scrollLimit) {
+                    // calculate the Y distance
+                    var scrollYDistance = document.activeElement.offsetHeight + (e.keyboardHeight - scrollLimit);
+                    // animate using move.min.js (CSS3 animations)
+                    //move(document.body).to(0, -scrollYDistance).duration('.2s').ease('in-out').end();
+                    $$(document.body).scrollTo(0,-scrollYDistance,200);
+                }
+            }
+
+            window.addEventListener('native.hidekeyboard', keyboardHideHandler);
+
+            // native.hidekeyboard callback
+            function keyboardHideHandler() {
+                StatusBar.hide();
+                // remove focus from activeElement
+                // which is naturally an input since the nativekeyboard is hiding
+                document.activeElement.blur();
+                // animate using move.min.js (CSS3 animations)
+                //move(document.body).to(0, 0).duration('.2s').ease('in-out').end();
+                $$(document.body).scrollTo(0,0,200);
+            }
+        }
+
+
 /*
         window.cordova.plugins.FileOpener.openFile("http://www.website.com/file.pdf",
             function(){
