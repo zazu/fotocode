631c94a82f70c8b8970aedacbc93bd85393b3f9e
zazu <ralf@goldenzazu.de>
2016-06-04 17:15:40 +0200

    config changes (android 4.1.2) error Ausgabe Barcode Scanner

--- js/camera.js
+++ js/camera.js
@@ -43,7 +43,7 @@ fc.camera =  {
     	if ( cam ) {
             cordova.plugins.barcodeScanner.scan(
                 function(result) {
-                    if ( result.format.length || result.text.length) {
+                    if ( result.format.length && result.text.length) {
                         success( result );
                     }
                     else if (result.cancelled )  {
